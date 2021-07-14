48ae3bd79f97fa3c3cee8986c1a57d413207d770
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2021-07-15 00:15:53 +0300

    Сделал 1е задание

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const listEl = document.querySelector('#categories');
+console.log(`В списке ${listEl.childElementCount} категории.`);
+
+[...listEl.children].forEach(li => {
+    console.log('Категория: ', li.firstElementChild.textContent);
+    console.log('Количество элементов: ', li.lastElementChild.childElementCount);
+    console.log('--------------');
+});
