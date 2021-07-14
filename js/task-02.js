4e1d2dfc3c7f33dc3dc4451ab653017cf7c1168d
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2021-07-15 00:49:00 +0300

    Сделал 2е задание

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+function createListItem(list) {
+  const markupItem = list.map(el => {
+    const itemEl = document.createElement('li');
+    itemEl.textContent = el;
+    return itemEl;
+  });
+
+  return markupItem;
+}
+const markup = createListItem(ingredients);
+const ul = document.querySelector("#ingredients");
+ul.append(...markup);
+
