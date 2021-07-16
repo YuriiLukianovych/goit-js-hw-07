f7fba428c56d01f683a7ae7524e6aedc50d7270e
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2021-07-16 09:39:15 +0300

    Поправил шаг ширины и высоты блоков в 8ой задаче

--- js/task-08.js
+++ js/task-08.js
@@ -4,38 +4,44 @@ const removeBtn = document.querySelector('[data-action="destroy"]');
 const box = document.querySelector('#boxes');
 
 removeBtn.addEventListener('click', () => {
-    inputNumber.value = '';
-    box.innerHTML = '';
+   inputNumber.value = '';
+   box.innerHTML = '';
 });
 
 createBtn.addEventListener('click', () => {
-    if ((inputNumber.value.length === 0) || (inputNumber.value === '0')) {
-        alert('Ведите значение в поле ввода');
-    } else if (Number(inputNumber.value) < Number(inputNumber.getAttribute('min'))){
-        inputNumber.value = '';
-        alert('Введите число больше нуля!');
-    } else if (Number(inputNumber.value) > Number(inputNumber.getAttribute('max'))) {
-        inputNumber.value = '';
-        alert('Введите число не больше 100');
-    }
-    const userValue = Number(inputNumber.value);
-    const markup = createBoxes(userValue);
-    box.append(...markup);
+   if (inputNumber.value.length === 0 || inputNumber.value === '0') {
+      alert('Ведите значение в поле ввода');
+   } else if (
+      Number(inputNumber.value) < Number(inputNumber.getAttribute('min'))
+   ) {
+      inputNumber.value = '';
+      alert('Введите число больше нуля!');
+   } else if (
+      Number(inputNumber.value) > Number(inputNumber.getAttribute('max'))
+   ) {
+      inputNumber.value = '';
+      alert('Введите число не больше 100');
+   }
+   const userValue = Number(inputNumber.value);
+   const markup = createBoxes(userValue);
+   box.append(...markup);
 });
 
 function createBoxes(amount) {
-    const arr = [];
-    let divWidth = 30;
-    let divHeight = 30;
-    for (let i = 0; i < amount; i++){
-        const markupItem = document.createElement('div');
-        const bgColor = '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6);
-        markupItem.style.backgroundColor = bgColor;
-        markupItem.style.width = `${divWidth}px`;
-        markupItem.style.height = `${divHeight}px`;
-        arr.push(markupItem);
-        divHeight += 20;
-        divWidth += 20;
-    }
-    return arr;
+   const arr = [];
+   let divWidth = 30;
+   let divHeight = 30;
+   for (let i = 0; i < amount; i++) {
+      console.log('hello');
+      const markupItem = document.createElement('div');
+      const bgColor =
+         '#' + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
+      markupItem.style.backgroundColor = bgColor;
+      markupItem.style.width = `${divWidth}px`;
+      markupItem.style.height = `${divHeight}px`;
+      arr.push(markupItem);
+      divHeight += 10;
+      divWidth += 10;
+   }
+   return arr;
 }
