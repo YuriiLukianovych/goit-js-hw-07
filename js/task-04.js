913b10d7e500ffb4912e9d252b5ac89edf26a315
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2021-07-16 19:22:57 +0300

    изменил название переменной с value на numValue

--- js/task-04.js
+++ js/task-04.js
@@ -1,47 +1,46 @@
 const refs = {
-    countBox: document.querySelector('#counter'),
-    btnDecr: document.querySelector('[data-action="decrement"]'),
-    btnIncr: document.querySelector('[data-action="increment"]'),
-    value: document.querySelector('#value'),
+   countBox: document.querySelector('#counter'),
+   btnDecr: document.querySelector('[data-action="decrement"]'),
+   btnIncr: document.querySelector('[data-action="increment"]'),
+   numValue: document.querySelector('#value'),
 };
 
 const cleanBtn = document.createElement('button');
 cleanBtn.style.display = 'block';
 cleanBtn.style.marginTop = '15px';
-cleanBtn.textContent = 'Сбросить'
+cleanBtn.textContent = 'Сбросить';
 refs.countBox.append(cleanBtn);
 
-let counterValue = Number(refs.value.textContent);
+let counterValue = Number(refs.numValue.textContent);
 
 refs.btnDecr.addEventListener('click', decrement);
 refs.btnIncr.addEventListener('click', increment);
 cleanBtn.addEventListener('click', cleanValue);
 
 function decrement() {
-    if (counterValue >= 1) {
-        // refs.btnDecr.disabled = false;
-        counterValue -= 1;
-        refs.value.textContent = counterValue.toString();
-        if (counterValue > 0) {
-            refs.btnDecr.disabled = false;
-        } else {
-            refs.btnDecr.disabled = true;
-        }
-    } 
+   if (counterValue >= 1) {
+      counterValue -= 1;
+      refs.numValue.textContent = counterValue.toString();
+      if (counterValue > 0) {
+         refs.btnDecr.disabled = false;
+      } else {
+         refs.btnDecr.disabled = true;
+      }
+   }
 }
 
-function increment(){
-    counterValue += 1;
-    refs.value.textContent = counterValue.toString();
-    if (counterValue > 0) {
-        refs.btnDecr.disabled = false;
-    } else {
-        refs.btnDecr.disabled = true;
-    }
+function increment() {
+   counterValue += 1;
+   refs.numValue.textContent = counterValue.toString();
+   if (counterValue > 0) {
+      refs.btnDecr.disabled = false;
+   } else {
+      refs.btnDecr.disabled = true;
+   }
 }
 
 function cleanValue() {
-    counterValue = 0;
-    refs.value.textContent = counterValue.toString();
-    refs.btnDecr.disabled = true;
+   counterValue = 0;
+   refs.numValue.textContent = counterValue.toString();
+   refs.btnDecr.disabled = true;
 }
