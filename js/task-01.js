7d138333870a9b896782e45987f2d43ae84b34ff
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2021-07-19 12:00:59 +0300

    Поправил мелочи

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
 const listEl = document.querySelector('#categories');
 console.log(`В списке ${listEl.childElementCount} категории.`);
 
-[...listEl.children].forEach(li => {
-    console.log('Категория: ', li.firstElementChild.textContent);
-    console.log('Количество элементов: ', li.lastElementChild.childElementCount);
-    console.log('--------------');
+listEl.children.forEach(li => {
+   console.log('Категория: ', li.firstElementChild.textContent);
+   console.log('Количество элементов: ', li.lastElementChild.childElementCount);
+   console.log('--------------');
 });
